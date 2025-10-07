f161902b42c080aefb2aa6185abf7369147622e5
Jimmeey2323 <155805143+Jimmeey2323@users.noreply.github.com>
2025-10-07 14:09:10 +0530

    Update useRecurringSessionsData.ts

--- src/hooks/useRecurringSessionsData.ts
+++ src/hooks/useRecurringSessionsData.ts
@@ -48,7 +48,7 @@ const GOOGLE_CONFIG = {
   TOKEN_URL: "https://oauth2.googleapis.com/token"
 };
 
-const SPREADSHEET_ID = "1i5GcTahIchSF6pLn0c2ZgqtQVYBG-Fqt7rGxIzD7qrA";
+const SPREADSHEET_ID = "149ILDqovzZA6FRUJKOwzutWdVqmqWBtWPfzG3A0zxTI";
 
 export const useRecurringSessionsData = () => {
   const [data, setData] = useState<RecurringSessionData[]>([]);
